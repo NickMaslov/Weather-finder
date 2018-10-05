ff4ec34c3b16fe19e2026de62d107c67b673cc17
Mykola Maslov <kategorychnost@gmail.com>
2018-10-05 04:30:04 -0400

    connected to Openweather API

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "9f1ccf064304405517d544fb078748d5";
+
 class App extends React.Component {
+
+getWeather = async (e) => {
+  e.preventDefault();
+  const city = e.target.elements.city.value;
+  const country = e.target.elements.country.value;
+
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${API_KEY}`);
+  const data = await api_call.json();
+  console.log(data)
+}
+
   render(){
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
